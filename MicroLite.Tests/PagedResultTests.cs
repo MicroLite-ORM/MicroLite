ffbb267c8decefea19714264feb89a4c46811111
Trevor Pilley <trev@trevorpilley.co.uk>
2012-05-24 22:01:43 +0100

    Added the tests for SqlQuery and PagedResult
    Missed from previous commit.

---
+++ MicroLite.Tests/PagedResultTests.cs
@@ -0,0 +1,31 @@
+﻿namespace MicroLite.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Unit Tests for the <see cref="PagedResult"/> class.
+    /// </summary>
+    [TestFixture]
+    public class PagedResultTests
+    {
+        [Test]
+        public void ConstructorSetsProperties()
+        {
+            var page = 1;
+            var results = new List<Customer> { new Customer() };
+
+            var pagedResults = new PagedResult<Customer>(page, results);
+
+            Assert.AreEqual(page, pagedResults.Page);
+            CollectionAssert.AreEqual(results, pagedResults.Results);
+        }
+
+        private class Customer
+        {
+        }
+    }
+}
---
+++ MicroLite.Tests/SqlQueryTests.cs
@@ -0,0 +1,24 @@
+﻿namespace MicroLite.Tests
+{
+    using System.Collections.ObjectModel;
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Unit Tests for the <see cref="SqlQuery"/> class.
+    /// </summary>
+    public class SqlQueryTests
+    {
+        [Test]
+        public void ConstructorSetsProperties()
+        {
+            var commandText = "SELECT * FROM Table WHERE Id = @p0";
+            var parameters = new object[] { 10 };
+
+            var sqlQuery = new SqlQuery(commandText, parameters);
+
+            Assert.AreEqual(commandText, sqlQuery.CommandText);
+            CollectionAssert.AreEqual(parameters, sqlQuery.Parameters);
+            Assert.IsInstanceOf<ReadOnlyCollection<object>>(sqlQuery.Parameters);
+        }
+    }
+}
