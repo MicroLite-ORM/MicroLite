6d85a41c0858cd7b399c8ed2ed085b10ef10f631
Trevor Pilley <trev@trevorpilley.co.uk>
2012-05-24 22:15:41 +0100

    Added the interfaces for fluent configuration

---
+++ MicroLite/Configuration/IConfigureConnection.cs
@@ -0,0 +1,30 @@
+﻿namespace MicroLite.Configuration
+{
+    /// <summary>
+    /// The interface which specifies the options for configuring the connection in the fluent configuration
+    /// of the MicroLite ORM framework.
+    /// </summary>
+    public interface IConfigureConnection : IHideObjectMembers
+    {
+        /// <summary>
+        /// Specifies the named connection string in the app config to be used.
+        /// </summary>
+        /// <param name="connectionName">The name of the connection string in the app config.</param>
+        /// <returns>The next step in the fluent configuration.</returns>
+        /// <exception cref="System.ArgumentNullException">Thrown if connectionName is null.</exception>
+        /// <exception cref="MicroLiteException">Thrown if the connection is not found in the app config.</exception>
+        /// <exception cref="System.NotSupportedException">Thrown if the provider name is not supported.</exception>
+        ICreateSessionFactory ForConnection(string connectionName);
+
+        /// <summary>
+        /// Specifies the connection string and provider name to be used.
+        /// </summary>
+        /// <param name="connectionString">The connection string.</param>
+        /// <param name="providerName">The provider name.</param>
+        /// <returns>The next step in the fluent configuration.</returns>
+        /// <exception cref="System.ArgumentNullException">Thrown if connectionString or providerName is null.</exception>
+        /// <exception cref="MicroLiteException">Thrown if the provider name is not installed.</exception>
+        /// <exception cref="System.NotSupportedException">Thrown if the provider name is not supported.</exception>
+        ICreateSessionFactory ForConnection(string connectionString, string providerName);
+    }
+}
---
+++ MicroLite/Configuration/ICreateSessionFactory.cs
@@ -0,0 +1,15 @@
+﻿namespace MicroLite.Configuration
+{
+    /// <summary>
+    /// The interface which specifies the creation of the <see cref="ISessionFactory"/> in the fluent configuration
+    /// of the MicroLite ORM framework.
+    /// </summary>
+    public interface ICreateSessionFactory : IHideObjectMembers
+    {
+        /// <summary>
+        /// Creates the session factory for the configured connection.
+        /// </summary>
+        /// <returns>The session factory for the specified connection.</returns>
+        ISessionFactory CreateSessionFactory();
+    }
+}
