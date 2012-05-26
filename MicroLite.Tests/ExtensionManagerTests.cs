c77fe8c779bea2891800d4d1ba4c7c9c26c14862
Trevor Pilley <trev@trevorpilley.co.uk>
2012-05-26 18:47:53 +0100

    Added ExtensionManager class
    Added the class which is used to manage extensions (IListeners).

---
+++ MicroLite.Tests/ExtensionManagerTests.cs
@@ -0,0 +1,47 @@
+﻿namespace MicroLite.Tests
+{
+    using System.Linq;
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Unit Tests for the <see cref="ExtensionManager"/> class.
+    /// </summary>
+    [TestFixture]
+    public class ExtensionManagerTests
+    {
+        [Test]
+        public void CreateListenersCallsFactoryCreatedByRegisterListenerT()
+        {
+            ExtensionManager.RegisterListener<TestListener>();
+
+            var listener = ExtensionManager.CreateListeners().Single();
+
+            Assert.IsInstanceOf<TestListener>(listener);
+        }
+
+        [Test]
+        public void RegisterListenerOnlyRegistersTypeOnce()
+        {
+            ExtensionManager.RegisterListener<TestListener>();
+            ExtensionManager.RegisterListener<TestListener>();
+
+            Assert.AreEqual(1, ExtensionManager.CreateListeners().Count());
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            ExtensionManager.ClearListeners();
+        }
+
+        [TestFixtureTearDown]
+        public void TearDown()
+        {
+            ExtensionManager.ClearListeners();
+        }
+
+        private class TestListener : Listener
+        {
+        }
+    }
+}
---
+++ MicroLite/ExtensionManager.cs
@@ -0,0 +1,49 @@
+﻿namespace MicroLite
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    /// <summary>
+    /// The class which manages extensions to the MicroLite ORM framework.
+    /// </summary>
+    internal static class ExtensionManager
+    {
+        private static readonly IList<Type> listenerTypes = new List<Type>();
+        private static readonly IList<Func<IListener>> listenerFactories = new List<Func<IListener>>();
+
+        /// <summary>
+        /// Registers the listener.
+        /// </summary>
+        /// <typeparam name="T">The type of <see cref="IListener"/> to register.</typeparam>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1004:GenericMethodsShouldProvideTypeParameter", Justification = "The syntax is accepted practice for registering types.")]
+        internal static void RegisterListener<T>()
+            where T : IListener, new()
+        {
+            var listenerType = typeof(T);
+
+            if (!listenerTypes.Contains(listenerType))
+            {
+                Func<IListener> listenerFactory = () =>
+                {
+                    return new T();
+                };
+
+                listenerFactories.Add(listenerFactory);
+
+                listenerTypes.Add(listenerType);
+            }
+        }
+
+        internal static IEnumerable<IListener> CreateListeners()
+        {
+            return listenerFactories.Select(x => x()).ToArray();
+        }
+
+        internal static void ClearListeners()
+        {
+            listenerFactories.Clear();
+            listenerTypes.Clear();
+        }
+    }
+}
