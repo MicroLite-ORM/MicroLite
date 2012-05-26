9c1a11fe9183c052d403abc687c195c1c96760a5
Trevor Pilley <trev@trevorpilley.co.uk>
2012-05-26 18:04:57 +0100

    Added IListener interface
    The IListener interface will initially be used to support DbAssigned
    identifier strategy, however going forward it will be extended to allow
    plugins to be written which intercept calls at certain points.

---
+++ MicroLite/IListener.cs
@@ -0,0 +1,35 @@
+﻿namespace MicroLite
+{
+    /// <summary>
+    /// The interface which exposes hooks into the processing of an object by the MicroLite ORM framework.
+    /// </summary>
+    internal interface IListener
+    {
+        /// <summary>
+        /// Invoked after the record for the instance has been inserted into the database.
+        /// </summary>
+        /// <param name="instance">The instance which has been inserted.</param>
+        /// <param name="executeScalarResult">The execute scalar result.</param>
+        void AfterInsert(object instance, object executeScalarResult);
+
+        /// <summary>
+        /// Invoked before the SqlQuery to insert the record into the database is created.
+        /// </summary>
+        /// <param name="instance">The instance to be inserted.</param>
+        /// <remarks>This is called before IListener.BeforeInsert(sqlQuery).</remarks>
+        void BeforeInsert(object instance);
+
+        /// <summary>
+        /// Invoked before the SqlQuery to insert the record into the database is executed.
+        /// </summary>
+        /// <param name="sqlQuery">The SqlQuery to be executed.</param>
+        /// <remarks>This is called after IListener.BeforeInsert(instance).</remarks>
+        void BeforeInsert(SqlQuery sqlQuery);
+
+        /// <summary>
+        /// Invoked before the SqlQuery to update the record in the database is created.
+        /// </summary>
+        /// <param name="instance">The instance to be updated.</param>
+        void BeforeUpdate(object instance);
+    }
+}
---
+++ MicroLite/Listener.cs
@@ -0,0 +1,45 @@
+﻿namespace MicroLite
+{
+    /// <summary>
+    /// Empty implementation of IListener.
+    /// </summary>
+    /// <remarks>
+    /// Provided so that implementations don't have to implement full interface.
+    /// Quite possibly a violation of SRP but it does allow for simplified extensions to the framework.
+    /// </remarks>
+    internal abstract class Listener : IListener
+    {
+        /// <summary>
+        /// Invoked after the record for the instance has been inserted into the database.
+        /// </summary>
+        /// <param name="instance">The instance which has been inserted.</param>
+        /// <param name="executeScalarResult">The execute scalar result.</param>
+        public virtual void AfterInsert(object instance, object executeScalarResult)
+        {
+        }
+
+        /// <summary>
+        /// Invoked before the SqlQuery to insert the record into the database is created.
+        /// </summary>
+        /// <param name="instance">The instance to be inserted.</param>
+        public virtual void BeforeInsert(object instance)
+        {
+        }
+
+        /// <summary>
+        /// Invoked before the SqlQuery to insert the record into the database is executed.
+        /// </summary>
+        /// <param name="sqlQuery">The SqlQuery to be executed.</param>
+        public virtual void BeforeInsert(SqlQuery sqlQuery)
+        {
+        }
+
+        /// <summary>
+        /// Invoked before the SqlQuery to update the record in the database is created.
+        /// </summary>
+        /// <param name="instance">The instance to be updated.</param>
+        public virtual void BeforeUpdate(object instance)
+        {
+        }
+    }
+}
