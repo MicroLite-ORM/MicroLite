cfd36ae296773cf4bd4b588004b91858cc3cdbdb
Trevor Pilley <trev@trevorpilley.co.uk>
2012-06-03 13:11:15 +0100

    Updated build to run NUnit tests

--- default.ps1
+++ default.ps1
@@ -1,11 +1,21 @@
-Task Default -depends Clean, Build
+properties {
+  $projectName = "MicroLite"
+  $base_dir = Resolve-Path .
+}
+
+Task Default -depends RunTests
+
+Task RunTests -Depends Build {
+  Write-Host "Running $projectName.Tests" -ForegroundColor Green
+  Exec {  & $base_dir\tools\nunit\nunit-console-x86.exe "$base_dir\$projectName.Tests\bin\Release\$projectName.Tests.dll" /nologo /nodots /noxml }
+}
 
 Task Build -Depends Clean {
-  Write-Host "Building MicroLite.sln" -ForegroundColor Green
-  Exec { msbuild "MicroLite.sln" /t:Build /p:Configuration=Release /v:quiet }
+  Write-Host "Building $projectName.sln" -ForegroundColor Green
+  Exec { msbuild "$projectName.sln" /t:Build /p:Configuration=Release /v:quiet }
 }
 
 Task Clean {
-  Write-Host "Cleaning MicroLite.sln" -ForegroundColor Green
-  Exec { msbuild "MicroLite.sln" /t:Clean /p:Configuration=Release /v:quiet }
+  Write-Host "Cleaning $projectName.sln" -ForegroundColor Green
+  Exec { msbuild "$projectName.sln" /t:Clean /p:Configuration=Release /v:quiet }
 }
