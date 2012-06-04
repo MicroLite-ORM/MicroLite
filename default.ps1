a707e37debf7f7f5b9967c915d0f86c782aa87fb
Trevor Pilley <trev@trevorpilley.co.uk>
2012-06-04 14:25:08 +0100

    Updated the build script
    Now outputs to the build directory

--- default.ps1
+++ default.ps1
@@ -1,18 +1,21 @@
 properties {
-  $projectName = "MicroLite"
   $base_dir = Resolve-Path .
+  $build_dir = "$base_dir\build\"
+  $projectName = "MicroLite"
 }
 
 Task Default -depends RunTests
 
 Task RunTests -Depends Build {
   Write-Host "Running $projectName.Tests" -ForegroundColor Green
-  Exec {  & $base_dir\tools\nunit\nunit-console-x86.exe "$base_dir\$projectName.Tests\bin\Release\$projectName.Tests.dll" /nologo /nodots /noxml }
+  Exec {  & $base_dir\tools\nunit\nunit-console-x86.exe "$build_dir\$projectName.Tests.dll" /nologo /nodots /noxml }
 }
 
 Task Build -Depends Clean {
+  Remove-Item -force -recurse $build_dir -ErrorAction SilentlyContinue
+
   Write-Host "Building $projectName.sln" -ForegroundColor Green
-  Exec { msbuild "$projectName.sln" /t:Build /p:Configuration=Release /v:quiet }
+  Exec { msbuild "$projectName.sln" /t:Build /p:"Configuration=Release;OutDir=$build_dir" /v:quiet }
 }
 
 Task Clean {
