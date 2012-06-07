7f742a4462d7b6ee97c33b1a993a3c0170014cf1
Trevor Pilley <trev@trevorpilley.co.uk>
2012-06-07 17:34:59 +0100

    Updated build script to build 3.5 and 4.0
    Builds against .net 3.5 Client Profile and .net 4.0 Client Profile

--- default.ps1
+++ default.ps1
@@ -1,25 +1,46 @@
 properties {
   $baseDir = Resolve-Path .
   $buildDir = "$baseDir\build"
+  $buildDir35 = "$buildDir\3.5\"
   $buildDir40 = "$buildDir\4.0\"
   $projectName = "MicroLite"
 }
 
-Task Default -depends RunTests
+Task Default -depends RunTests, Build35, Build40
+
+Task Build35 {
+  Remove-Item -force -recurse $buildDir35 -ErrorAction SilentlyContinue
+  
+  Write-Host "Cleaning $projectName.csproj" -ForegroundColor Green
+  Exec { msbuild "$projectName\$projectName.csproj" /t:Clean /p:Configuration=Release /v:quiet }
+ 
+  Write-Host "Building $projectName.csproj for .net 3.5" -ForegroundColor Green
+  Exec { msbuild "$projectName\$projectName.csproj" /target:Build "/property:Configuration=Release;OutDir=$buildDir35;TargetFrameworkVersion=v3.5;TargetFrameworkProfile=Client" /verbosity:quiet }
+}
+
+Task Build40 {
+  Remove-Item -force -recurse $buildDir40 -ErrorAction SilentlyContinue
+  
+  Write-Host "Cleaning $projectName.csproj" -ForegroundColor Green
+  Exec { msbuild "$projectName\$projectName.csproj" /t:Clean /p:Configuration=Release /v:quiet }
+ 
+  Write-Host "Building $projectName.csproj for .net 4.0" -ForegroundColor Green
+  Exec { msbuild "$projectName\$projectName.csproj" /target:Build "/property:Configuration=Release;OutDir=$buildDir40;TargetFrameworkVersion=v4.0;TargetFrameworkProfile=Client" /verbosity:quiet }
+}
 
 Task RunTests -Depends Build {
   Write-Host "Running $projectName.Tests" -ForegroundColor Green
-  Exec {  & $baseDir\tools\nunit\nunit-console-x86.exe "$buildDir40\$projectName.Tests.dll" /nologo /nodots /noxml }
+  Exec {  & $baseDir\tools\nunit\nunit-console-x86.exe "$baseDir\MicroLite.Tests\bin\Release\$projectName.Tests.dll" /nologo /nodots /noxml }
 }
 
 Task Build -Depends Clean {
-  Remove-Item -force -recurse $buildDir -ErrorAction SilentlyContinue
- 
-  Write-Host "Building $projectName.sln for .net 4.0" -ForegroundColor Green
-  Exec { msbuild "$projectName.sln" /target:Build "/property:Configuration=Release;OutDir=$buildDir40;TargetFrameworkVersion=v4.0" /verbosity:quiet }
+  Write-Host "Building $projectName.sln" -ForegroundColor Green
+  Exec { msbuild "$projectName.sln" /target:Build /property:Configuration=Release /verbosity:quiet }  
 }
 
 Task Clean {
+  Remove-Item -force -recurse $buildDir -ErrorAction SilentlyContinue
+
   Write-Host "Cleaning $projectName.sln" -ForegroundColor Green
   Exec { msbuild "$projectName.sln" /t:Clean /p:Configuration=Release /v:quiet }
 }
