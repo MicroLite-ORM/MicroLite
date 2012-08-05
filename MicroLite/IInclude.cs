d2d899168381c114d35dcdaf2e1a307a571c7311
Trevor Pilley <trev@trevorpilley.co.uk>
2012-08-05 15:23:24 +0100

    Added the interfaces for includes

---
+++ MicroLite/IInclude.cs
@@ -0,0 +1,33 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="IInclude.cs" company="MicroLite">
+// Copyright 2012 Trevor Pilley
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// </copyright>
+// -----------------------------------------------------------------------
+namespace MicroLite
+{
+    /// <summary>
+    /// The interface for including a single result.
+    /// </summary>
+    /// <typeparam name="T">The type of object to be included.</typeparam>
+    public interface IInclude<T>
+    {
+        /// <summary>
+        /// Gets the included value.
+        /// </summary>
+        /// <value>
+        /// If the overall query has not been executed, or if the query did not return a result the value will be null.
+        /// Otherwise the result of the query.
+        /// </value>
+        T Value
+        {
+            get;
+        }
+    }
+}
---
+++ MicroLite/ISessionInclude.cs
@@ -0,0 +1,59 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ISessionInclude.cs" company="MicroLite">
+// Copyright 2012 Trevor Pilley
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// </copyright>
+// -----------------------------------------------------------------------
+namespace MicroLite
+{
+    using System;
+
+    /// <summary>
+    /// The interface which provides access to include operations.
+    /// </summary>
+    /// <remarks>
+    /// These operations allow for batch included values and have been moved to a separate interface to avoid
+    /// cluttering the ISession API.
+    /// </remarks>
+    public interface ISessionInclude : IHideObjectMethods
+    {
+        /// <summary>
+        /// Includes many instances based upon the specified SQL query.
+        /// </summary>
+        /// <typeparam name="T">The type of object.</typeparam>
+        /// <param name="sqlQuery">The SQL query to execute.</param>
+        /// <returns>A pointer to the included instances of the specified type.</returns>
+        IIncludeMany<T> Many<T>(SqlQuery sqlQuery) where T : class, new();
+
+        /// <summary>
+        /// Includes the instance of the specified type which corresponds to the row with the specified identifier
+        /// in the mapped table.
+        /// </summary>
+        /// <typeparam name="T">The type of object.</typeparam>
+        /// <param name="identifier">The record identifier.</param>
+        /// <returns>A pointer to the included instance of the specified type.</returns>
+        /// <exception cref="ObjectDisposedException">Thrown if the session has been disposed.</exception>
+        /// <exception cref="ArgumentNullException">Thrown if the specified instance is null.</exception>
+        /// <exception cref="MicroLiteException">Thrown if there is an error executing the query.</exception>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1716:IdentifiersShouldNotMatchKeywords", MessageId = "Single", Justification = "It's used in loads of places by the linq extension methods as a method name.")]
+        IInclude<T> Single<T>(object identifier) where T : class, new();
+
+        /// <summary>
+        /// Includes a single instance based upon the specified SQL query.
+        /// </summary>
+        /// <typeparam name="T">The type of object.</typeparam>
+        /// <param name="sqlQuery">The SQL query to execute.</param>
+        /// <returns>A pointer to the included instance of the specified type.</returns>
+        /// <exception cref="ObjectDisposedException">Thrown if the session has been disposed.</exception>
+        /// <exception cref="ArgumentNullException">Thrown if the specified instance is null.</exception>
+        /// <exception cref="MicroLiteException">Thrown if there is an error executing the query.</exception>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1716:IdentifiersShouldNotMatchKeywords", MessageId = "Single", Justification = "It's used in loads of places by the linq extension methods as a method name.")]
+        IInclude<T> Single<T>(SqlQuery sqlQuery) where T : class, new();
+    }
+}
