f832c5ed5ae2c23efb01c60c8ce49d459f72d970
Trevor Pilley <trev@trevorpilley.co.uk>
2012-08-05 15:29:33 +0100

    Added the implementation for include many

---
+++ MicroLite.Tests/Core/IncludeManyTests.cs
@@ -0,0 +1,71 @@
+﻿namespace MicroLite.Tests.Core
+{
+    using System.Collections.Generic;
+    using System.Data;
+    using MicroLite.Core;
+    using MicroLite.Mapping;
+    using Moq;
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Unit Tests for the <see cref="IncludeMany&lt;T&gt;"/> class.
+    /// </summary>
+    [TestFixture]
+    public class IncludeManyTests
+    {
+        [Test]
+        public void ValuesReturnsEmptyListIfNoResultsInReader()
+        {
+            var mockReader = new Mock<IDataReader>();
+            mockReader.Setup(x => x.Read()).Returns(new Queue<bool>(new[] { false }).Dequeue);
+
+            var include = new IncludeMany<Customer>();
+
+            include.BuildValue(mockReader.Object, new Mock<IObjectBuilder>().Object);
+
+            CollectionAssert.IsEmpty(include.Values);
+        }
+
+        [Test]
+        public void ValuesReturnsNullIfNuildValueNotCalled()
+        {
+            var include = new IncludeMany<Customer>();
+
+            Assert.IsNull(include.Values);
+        }
+
+        [Test]
+        public void ValuesReturnsResults()
+        {
+            var mockReader = new Mock<IDataReader>();
+            mockReader.Setup(x => x.Read()).Returns(new Queue<bool>(new[] { true, false }).Dequeue);
+
+            var reader = mockReader.Object;
+
+            var mockObjectBuilder = new Mock<IObjectBuilder>();
+            mockObjectBuilder.Setup(x => x.BuildInstance<Customer>(It.IsAny<ObjectInfo>(), reader)).Returns(new Customer());
+
+            var include = new IncludeMany<Customer>();
+
+            include.BuildValue(mockReader.Object, mockObjectBuilder.Object);
+
+            mockReader.VerifyAll();
+            mockObjectBuilder.VerifyAll();
+
+            CollectionAssert.IsNotEmpty(include.Values);
+            CollectionAssert.AllItemsAreInstancesOfType(include.Values, typeof(Customer));
+        }
+
+        [MicroLite.Mapping.Table("dbo", "Customers")]
+        private class Customer
+        {
+            [MicroLite.Mapping.Column("CustomerId")]
+            [MicroLite.Mapping.Identifier(MicroLite.Mapping.IdentifierStrategy.Identity)]
+            public int Id
+            {
+                get;
+                set;
+            }
+        }
+    }
+}
---
+++ MicroLite/Core/IncludeMany.cs
@@ -0,0 +1,49 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="IncludeMany.cs" company="MicroLite">
+// Copyright 2012 Trevor Pilley
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// </copyright>
+// -----------------------------------------------------------------------
+namespace MicroLite.Core
+{
+    using System.Collections.Generic;
+    using System.Data;
+    using MicroLite.Mapping;
+
+    /// <summary>
+    /// The default implementation of <see cref="IIncludeMany&lt;T&gt;"/>.
+    /// </summary>
+    /// <typeparam name="T">The type of object to be included.</typeparam>
+    internal sealed class IncludeMany<T> : Include, IIncludeMany<T> where T : class, new()
+    {
+        private IList<T> values;
+
+        public IList<T> Values
+        {
+            get
+            {
+                return this.values;
+            }
+        }
+
+        internal override void BuildValue(IDataReader reader, IObjectBuilder objectBuilder)
+        {
+            this.values = new List<T>();
+
+            var objectInfo = ObjectInfo.For(typeof(T));
+
+            while (reader.Read())
+            {
+                var value = objectBuilder.BuildInstance<T>(objectInfo, reader);
+
+                this.values.Add(value);
+            }
+        }
+    }
+}
