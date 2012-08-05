cb198f3f2f328c6db29549f6e771e44d1cf10a4d
Trevor Pilley <trev@trevorpilley.co.uk>
2012-08-05 15:24:41 +0100

    Added the base class for includes

---
+++ MicroLite/Core/Include.cs
@@ -0,0 +1,29 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Include.cs" company="MicroLite">
+// Copyright 2012 Trevor Pilley
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// </copyright>
+// -----------------------------------------------------------------------
+namespace MicroLite.Core
+{
+    using System.Data;
+
+    /// <summary>
+    /// The base class for include implementations.
+    /// </summary>
+    internal abstract class Include
+    {
+        /// <summary>
+        /// Builds the included value from the results in the data reader using the supplied object builder.
+        /// </summary>
+        /// <param name="reader">The <see cref="IDataReader"/> containing the results.</param>
+        /// <param name="objectBuilder">The object builder to use to build the included value.</param>
+        internal abstract void BuildValue(IDataReader reader, IObjectBuilder objectBuilder);
+    }
+}
