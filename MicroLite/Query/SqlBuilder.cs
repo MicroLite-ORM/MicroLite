156b9cd85970941fe7b3bed0b80ff145b046b786
Trevor Pilley <trev@trevorpilley.co.uk>
2012-08-28 17:57:49 +0100

    Moved Combine SqlQueries to new SqlUtil class

--- MicroLite/Query/SqlBuilder.cs
+++ MicroLite/Query/SqlBuilder.cs
@@ -14,10 +14,8 @@ namespace MicroLite.Query
 {
     using System;
     using System.Collections.Generic;
-    using System.Globalization;
     using System.Linq;
     using System.Text;
-    using System.Text.RegularExpressions;
     using MicroLite.Mapping;
 
     /// <summary>
@@ -25,7 +23,6 @@ namespace MicroLite.Query
     /// </summary>
     public sealed class SqlBuilder : IFrom, IFunction, IWhereOrOrderBy, IAndOrOrderBy, IOrderBy, IToSqlQuery, IWithParameter
     {
-        private static readonly Regex parameterRegex = new Regex(@"(@p\d)", RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Singleline | RegexOptions.Multiline);
         private readonly List<object> arguments = new List<object>();
         private readonly StringBuilder innerSql = new StringBuilder();
 
@@ -39,34 +36,6 @@ namespace MicroLite.Query
             {
                 this.innerSql.Append(startingSql);
             }
-        }
-
-        /// <summary>
-        /// Combines the specified SQL queries into a single SqlQuery.
-        /// </summary>
-        /// <param name="sqlQueries">The SQL queries to be combined.</param>
-        /// <returns>The combined <see cref="SqlQuery" />.</returns>
-        /// <exception cref="System.ArgumentNullException">Thrown if sqlQueries is null.</exception>
-        public static SqlQuery Combine(IEnumerable<SqlQuery> sqlQueries)
-        {
-            if (sqlQueries == null)
-            {
-                throw new ArgumentNullException("sqlQueries");
-            }
-
-            int argumentsCount = 0;
-            var sqlBuilder = new StringBuilder();
-
-            foreach (var sqlQuery in sqlQueries)
-            {
-                argumentsCount += sqlQuery.Arguments.Count;
-
-                var sql = ReNumberParameters(sqlQuery.CommandText, argumentsCount);
-
-                sqlBuilder.AppendLine(sql + ";");
-            }
-
-            return new SqlQuery(sqlBuilder.ToString(0, sqlBuilder.Length - 3), sqlQueries.SelectMany(s => s.Arguments).ToArray());
         }
 
         /// <summary>
@@ -233,40 +202,11 @@ namespace MicroLite.Query
             return this;
         }
 
-        /// <summary>
-        /// Re-numbers the parameters in the SQL based upon the total number of arguments.
-        /// </summary>
-        /// <param name="sql">The SQL.</param>
-        /// <param name="argumentsCount">The number of arguments.</param>
-        /// <returns>The re-numbered SQL</returns>
-        private static string ReNumberParameters(string sql, int argumentsCount)
-        {
-            var argsAdded = 0;
-
-            var predicateReWriter = new StringBuilder(sql);
-
-            var parameterNames = new HashSet<string>(parameterRegex.Matches(sql).Cast<Match>().Select(x => x.Value));
-
-            if (parameterNames.Count > 0)
-            {
-                var parameterPrefix = parameterNames.First().Substring(0, 2);
-
-                foreach (var parameterName in parameterNames.OrderByDescending(n => n))
-                {
-                    var newParameterName = parameterPrefix + (argumentsCount - ++argsAdded).ToString(CultureInfo.InvariantCulture);
-
-                    predicateReWriter.Replace(parameterName, newParameterName);
-                }
-            }
-
-            return predicateReWriter.ToString();
-        }
-
         private void AppendPredicate(string appendFormat, string predicate, params object[] args)
         {
             this.arguments.AddRange(args);
 
-            var renumberedPredicate = ReNumberParameters(predicate, this.arguments.Count);
+            var renumberedPredicate = SqlUtil.ReNumberParameters(predicate, this.arguments.Count);
 
             this.innerSql.AppendFormat(appendFormat, renumberedPredicate);
             this.innerSql.AppendLine();
---
+++ MicroLite/SqlUtil.cs
@@ -0,0 +1,86 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="SqlUtil.cs" company="MicroLite">
+// Copyright 2012 Trevor Pilley
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// </copyright>
+// -----------------------------------------------------------------------
+namespace MicroLite
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Linq;
+    using System.Text;
+    using System.Text.RegularExpressions;
+
+    /// <summary>
+    /// A utility class containing useful methods for manipulating Sql.
+    /// </summary>
+    internal static class SqlUtil
+    {
+        private static readonly Regex parameterRegex = new Regex(@"(@p\d)", RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Singleline | RegexOptions.Multiline);
+
+        /// <summary>
+        /// Combines the specified SQL queries into a single SqlQuery.
+        /// </summary>
+        /// <param name="sqlQueries">The SQL queries to be combined.</param>
+        /// <returns>The combined <see cref="SqlQuery" />.</returns>
+        /// <exception cref="System.ArgumentNullException">Thrown if sqlQueries is null.</exception>
+        internal static SqlQuery Combine(IEnumerable<SqlQuery> sqlQueries)
+        {
+            if (sqlQueries == null)
+            {
+                throw new ArgumentNullException("sqlQueries");
+            }
+
+            int argumentsCount = 0;
+            var sqlBuilder = new StringBuilder();
+
+            foreach (var sqlQuery in sqlQueries)
+            {
+                argumentsCount += sqlQuery.Arguments.Count;
+
+                var sql = ReNumberParameters(sqlQuery.CommandText, argumentsCount);
+
+                sqlBuilder.AppendLine(sql + ";");
+            }
+
+            return new SqlQuery(sqlBuilder.ToString(0, sqlBuilder.Length - 3), sqlQueries.SelectMany(s => s.Arguments).ToArray());
+        }
+
+        /// <summary>
+        /// Re-numbers the parameters in the SQL based upon the total number of arguments.
+        /// </summary>
+        /// <param name="sql">The SQL.</param>
+        /// <param name="argumentsCount">The total number of arguments.</param>
+        /// <returns>The re-numbered SQL</returns>
+        internal static string ReNumberParameters(string sql, int argumentsCount)
+        {
+            var argsAdded = 0;
+
+            var predicateReWriter = new StringBuilder(sql);
+
+            var parameterNames = new HashSet<string>(parameterRegex.Matches(sql).Cast<Match>().Select(x => x.Value));
+
+            if (parameterNames.Count > 0)
+            {
+                var parameterPrefix = parameterNames.First().Substring(0, 2);
+
+                foreach (var parameterName in parameterNames.OrderByDescending(n => n))
+                {
+                    var newParameterName = parameterPrefix + (argumentsCount - ++argsAdded).ToString(CultureInfo.InvariantCulture);
+
+                    predicateReWriter.Replace(parameterName, newParameterName);
+                }
+            }
+
+            return predicateReWriter.ToString();
+        }
+    }
+}
