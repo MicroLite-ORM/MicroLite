d37d72a6bc19e7e3a205ce93e84f65cbfaa6042a
Trevor Pilley <trev@trevorpilley.co.uk>
2012-09-01 20:40:23 +0100

    Consolidated build process further
    Using the built in nuget build integration instead of a pre-build step.

--- build.ps1
+++ build.ps1
@@ -1,7 +1,39 @@
-$scriptPath = Split-Path $MyInvocation.InvocationName
+param(
+  [string]$version,
+  [bool]$push
+)
 
+$projectName = "MicroLite"
+
+$scriptPath = Split-Path $MyInvocation.InvocationName
+$buildDir = "$scriptPath\build"
+$nuGetExe = "$scriptPath\.nuget\NuGet.exe"
+$nuSpec = "$scriptPath\$projectName.nuspec"
+$nuGetPackage = "$buildDir\$projectName.$version.nupkg"
+
+# Run the psake build script to create the release binaries
 Import-Module (Join-Path $scriptPath tools\psake\psake.psm1) -ErrorAction SilentlyContinue
 
 Invoke-psake (Join-Path $scriptPath default.ps1)
 
 Remove-Module psake -ErrorAction SilentlyContinue
+
+if ($version)
+{
+	Write-Host "Update NuGet.exe" -ForegroundColor Green
+	& $nuGetExe Update -self
+
+	if (Test-Path "$nuGetExe.old")
+	{
+  		Remove-Item -force "$nuGetExe.old" -ErrorAction SilentlyContinue
+	}
+
+	Write-Host "Pack $nuSpec -> $nuGetPackage" -ForegroundColor Green
+	& $nuGetExe Pack $nuSpec -Version $version -OutputDirectory $buildDir -BasePath $buildDir
+
+	if($push)
+	{
+		Write-Host "Push $nuGetPackage -> http://nuget.org" -ForegroundColor Green
+		& $nuGetExe Push $nuGetPackage
+	}
+}
