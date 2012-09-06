2193c6dd2a2bc0aafd35ca7207a843ef6585137c
Trevor Pilley <trev@trevorpilley.co.uk>
2012-09-06 17:56:58 +0100

    Added examples to ISession for #86

--- MicroLite/ISession.cs
+++ MicroLite/ISession.cs
@@ -49,6 +49,29 @@ namespace MicroLite
         /// Begins the transaction with the default isolation level of the connection.
         /// </summary>
         /// <returns>An <see cref="ITransaction"/> with the default isolation level of the connection.</returns>
+        /// <remarks>It is a good idea to perform all insert/update/delete actions inside a transaction.</remarks>
+        /// <example>
+        /// <code>
+        /// using (var session = sessionFactory.OpenSession())
+        /// {
+        ///     using (var transaction = session.BeginTransaction())
+        ///     {
+        ///         // perform actions against ISession.
+        ///         // ...
+        ///
+        ///         try
+        ///         {
+        ///             transaction.Commit();
+        ///         }
+        ///         catch (Exception exception)
+        ///         {
+        ///             transaction.Rollback();
+        ///             // Log or throw the exception.
+        ///         }
+        ///     }
+        /// }
+        /// </code>
+        /// </example>
         ITransaction BeginTransaction();
 
         /// <summary>
@@ -56,6 +79,30 @@ namespace MicroLite
         /// </summary>
         /// <param name="isolationLevel">The isolation level to use for the transaction.</param>
         /// <returns>An <see cref="ITransaction"/> with the specified <see cref="IsolationLevel"/>.</returns>
+        /// <remarks>It is a good idea to perform all insert/update/delete actions inside a transaction.</remarks>
+        /// <example>
+        /// <code>
+        /// using (var session = sessionFactory.OpenSession())
+        /// {
+        ///     // This overload allows us to specify a specific IsolationLevel.
+        ///     using (var transaction = session.BeginTransaction(IsolationLevel.ReadCommitted))
+        ///     {
+        ///         // perform actions against ISession.
+        ///         // ...
+        ///
+        ///         try
+        ///         {
+        ///             transaction.Commit();
+        ///         }
+        ///         catch (Exception exception)
+        ///         {
+        ///             transaction.Rollback();
+        ///             // Log or throw the exception.
+        ///         }
+        ///     }
+        /// }
+        /// </code>
+        /// </example>
         ITransaction BeginTransaction(IsolationLevel isolationLevel);
 
         /// <summary>
@@ -66,6 +113,31 @@ namespace MicroLite
         /// <exception cref="ObjectDisposedException">Thrown if the session has been disposed.</exception>
         /// <exception cref="ArgumentNullException">Thrown if the specified instance is null.</exception>
         /// <exception cref="MicroLiteException">Thrown if there is an error executing the delete command.</exception>
+        /// <example>
+        /// <code>
+        /// bool deleted = false;
+        ///
+        /// using (var session = sessionFactory.OpenSession())
+        /// {
+        ///     using (var transaction = session.BeginTransaction())
+        ///     {
+        ///         try
+        ///         {
+        ///             deleted = session.Delete(customer);
+        ///
+        ///             transaction.Commit();
+        ///         }
+        ///         catch
+        ///         {
+        ///             deleted = false;
+        ///
+        ///             transaction.Rollback();
+        ///             // Log or throw the exception.
+        ///         }
+        ///     }
+        /// }
+        /// </code>
+        /// </example>
         bool Delete(object instance);
 
         /// <summary>
@@ -77,6 +149,16 @@ namespace MicroLite
         /// <exception cref="ObjectDisposedException">Thrown if the session has been disposed.</exception>
         /// <exception cref="ArgumentNullException">Thrown if the specified SqlQuery is null.</exception>
         /// <exception cref="MicroLiteException">Thrown if there is an error executing the query.</exception>
+        /// <example>
+        /// <code>
+        /// using (var session = sessionFactory.OpenSession())
+        /// {
+        ///     var query = new SqlQuery("SELECT * FROM Invoices WHERE CustomerId = @p0", 1324);
+        ///
+        ///     var invoices = session.Fetch&lt;Invoice&gt;(query);
+        /// }
+        /// </code>
+        /// </example>
         IList<T> Fetch<T>(SqlQuery sqlQuery) where T : class, new();
 
         /// <summary>
@@ -86,6 +168,27 @@ namespace MicroLite
         /// <exception cref="ObjectDisposedException">Thrown if the session has been disposed.</exception>
         /// <exception cref="ArgumentNullException">Thrown if the specified instance is null.</exception>
         /// <exception cref="MicroLiteException">Thrown if there is an error executing the insert command.</exception>
+        /// <example>
+        /// <code>
+        /// using (var session = sessionFactory.OpenSession())
+        /// {
+        ///     using (var transaction = session.BeginTransaction())
+        ///     {
+        ///         try
+        ///         {
+        ///             session.Insert(customer);
+        ///
+        ///             transaction.Commit();
+        ///         }
+        ///         catch
+        ///         {
+        ///             transaction.Rollback();
+        ///             // Log or throw the exception.
+        ///         }
+        ///     }
+        /// }
+        /// </code>
+        /// </example>
         void Insert(object instance);
 
         /// <summary>
@@ -100,6 +203,16 @@ namespace MicroLite
         /// <exception cref="ArgumentNullException">Thrown if the specified SqlQuery is null.</exception>
         /// <exception cref="ArgumentOutOfRangeException">Thrown if page or resultsPerPage are below 1.</exception>
         /// <exception cref="MicroLiteException">Thrown if there is an error executing the query.</exception>
+        /// <example>
+        /// <code>
+        /// using (var session = sessionFactory.OpenSession())
+        /// {
+        ///     var query = new SqlQuery("SELECT * FROM Customers WHERE LastName = @p0", "Smith");
+        ///
+        ///     var customers = session.Paged&lt;Customer&gt;(query, page: 1, resultsPerPage: 25);
+        /// }
+        /// </code>
+        /// </example>
         PagedResult<T> Paged<T>(SqlQuery sqlQuery, long page, long resultsPerPage) where T : class, new();
 
         /// <summary>
@@ -112,11 +225,19 @@ namespace MicroLite
         /// <exception cref="ObjectDisposedException">Thrown if the session has been disposed.</exception>
         /// <exception cref="ArgumentNullException">Thrown if the specified instance is null.</exception>
         /// <exception cref="MicroLiteException">Thrown if there is an error executing the query.</exception>
+        /// <example>
+        /// <code>
+        /// using (var session = sessionFactory.OpenSession())
+        /// {
+        ///     var customer = session.Single&lt;Customer&gt;(17867);
+        /// }
+        /// </code>
+        /// </example>
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1716:IdentifiersShouldNotMatchKeywords", MessageId = "Single", Justification = "It's used in loads of places by the linq extension methods as a method name.")]
         T Single<T>(object identifier) where T : class, new();
 
         /// <summary>
-        /// Returns a single instance based upon the specified SQL query, or null if no results are returned.
+        /// Returns a single instance based upon the specified SQL query, or null if no result is returned.
         /// </summary>
         /// <typeparam name="T">The type of object.</typeparam>
         /// <param name="sqlQuery">The SQL query to execute.</param>
@@ -124,6 +245,17 @@ namespace MicroLite
         /// <exception cref="ObjectDisposedException">Thrown if the session has been disposed.</exception>
         /// <exception cref="ArgumentNullException">Thrown if the specified instance is null.</exception>
         /// <exception cref="MicroLiteException">Thrown if there is an error executing the query.</exception>
+        /// <example>
+        /// <code>
+        /// using (var session = sessionFactory.OpenSession())
+        /// {
+        ///     var query = new SqlQuery("SELECT * FROM Customers WHERE EmailAddress = @p0", "fred.flintstone@bedrock.com");
+        ///
+        ///     // This overload is useful to retrieve a single object based upon a unique value which isn't it's identifier.
+        ///     var customer = session.Single&lt;Customer&gt;(query);
+        /// }
+        /// </code>
+        /// </example>
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1716:IdentifiersShouldNotMatchKeywords", MessageId = "Single", Justification = "It's used in loads of places by the linq extension methods as a method name.")]
         T Single<T>(SqlQuery sqlQuery) where T : class, new();
 
@@ -134,6 +266,27 @@ namespace MicroLite
         /// <exception cref="ObjectDisposedException">Thrown if the session has been disposed.</exception>
         /// <exception cref="ArgumentNullException">Thrown if the specified instance is null.</exception>
         /// <exception cref="MicroLiteException">Thrown if there is an error executing the update command.</exception>
+        /// <example>
+        /// <code>
+        /// using (var session = sessionFactory.OpenSession())
+        /// {
+        ///     using (var transaction = session.BeginTransaction())
+        ///     {
+        ///         try
+        ///         {
+        ///             session.Update(customer);
+        ///
+        ///             transaction.Commit();
+        ///         }
+        ///         catch
+        ///         {
+        ///             transaction.Rollback();
+        ///             // Log or throw the exception.
+        ///         }
+        ///     }
+        /// }
+        /// </code>
+        /// </example>
         void Update(object instance);
     }
 }
