07445759cd543c290e048290d4213b43edbfe0ac
Trevor Pilley <trev@trevorpilley.co.uk>
2013-01-02 20:31:35 +0000

    Assembly version should still be x.x.x.x

--- build.ps1
+++ build.ps1
@@ -16,7 +16,7 @@ function UpdateAssemblyInfoFiles ([string] $buildVersion)
 	$assemblyVersionPattern = 'AssemblyVersion\("[0-9]+(\.([0-9]+|\*)){1,3}"\)'
 	$fileVersionPattern = 'AssemblyFileVersion\("[0-9]+(\.([0-9]+|\*)){1,3}"\)'
 	$infoVersionPattern = 'AssemblyInformationalVersion\("[0-9]+(\.([0-9]+|\*)){1,3}(.*)"\)'
-	$assemblyVersion = 'AssemblyVersion("' + $buildVersion.SubString(0, 3) + '.0")';
+	$assemblyVersion = 'AssemblyVersion("' + $buildVersion.SubString(0, 3) + '.0.0")';
 	$fileVersion = 'AssemblyFileVersion("' + $buildVersion.SubString(0, 5) + '.0")';
 	$infoVersion = 'AssemblyInformationalVersion("' + $buildVersion + '")';
 	
