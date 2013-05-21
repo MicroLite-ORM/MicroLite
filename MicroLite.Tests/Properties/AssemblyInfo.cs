6aa953f9be77e66903c607a864fe1ebed6cf60dc
Trevor Pilley <trev@trevorpilley.co.uk>
2013-05-21 18:24:21 +0100

    Built 3.1.1
    No actual changes but I messed up the nuget package and 3.1.0 didn't
    contain the correct binaries. Since there is no way to delete/replace a
    nuget package once uploaded I had to create a new one.

--- MicroLite.Tests/Properties/AssemblyInfo.cs
+++ MicroLite.Tests/Properties/AssemblyInfo.cs
@@ -30,6 +30,6 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 [assembly: AssemblyVersion("3.1.0.0")]
-[assembly: AssemblyFileVersion("3.1.0.0")]
+[assembly: AssemblyFileVersion("3.1.1.0")]
 [assembly: CLSCompliant(true)]
 [assembly: NeutralResourcesLanguageAttribute("en-GB")]
