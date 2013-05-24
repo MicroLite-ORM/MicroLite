08eb69b9657393d0f9902bf60974769703d0ef80
Trevor Pilley <trev@trevorpilley.co.uk>
2013-05-24 18:13:54 +0100

    Updated copyright headers

--- MicroLite/TypeConverters/EnumTypeConverter.cs
+++ MicroLite/TypeConverters/EnumTypeConverter.cs
@@ -1,6 +1,6 @@
 ﻿// -----------------------------------------------------------------------
 // <copyright file="EnumTypeConverter.cs" company="MicroLite">
-// Copyright 2012 Trevor Pilley
+// Copyright 2012 - 2013 Trevor Pilley
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
