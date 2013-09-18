47706a0c8b36fec2618087e40e530ea253b04ed7
Trevor Pilley <trev@trevorpilley.co.uk>
2013-09-18 20:19:20 +0100

    Updated copright

--- MicroLite/SqlCharacters.cs
+++ MicroLite/SqlCharacters.cs
@@ -1,6 +1,6 @@
 ﻿// -----------------------------------------------------------------------
 // <copyright file="SqlCharacters.cs" company="MicroLite">
-// Copyright 2012 - 2013 Trevor Pilley
+// Copyright 2012 - 2013 Project Contributors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
