6ce412d8ab59f504b37906e03a14143e773bfa48
Trevor Pilley <trev@trevorpilley.co.uk>
2014-01-21 18:10:51 +0000

    Removed unnecessary test
    This is left over from when we had the Add<TListener> method on the
    collection.

--- MicroLite.Tests/Listeners/ListenerCollectionTests.cs
+++ MicroLite.Tests/Listeners/ListenerCollectionTests.cs
@@ -35,32 +35,5 @@
                 Assert.Equal(2, this.collection.Count);
             }
         }
-
-        public class WhenEnumerating
-        {
-            private readonly ListenerCollection collection = new ListenerCollection();
-            private readonly IListener listener1;
-            private readonly IListener listener2;
-
-            public WhenEnumerating()
-            {
-                collection.Clear();
-
-                collection.Add(new TestListener());
-
-                listener1 = collection.Single();
-                listener2 = collection.Single();
-            }
-
-            [Fact]
-            public void TheSameInstanceShouldBeReturned()
-            {
-                Assert.Same(listener1, listener2);
-            }
-        }
-
-        private class TestListener : Listener
-        {
-        }
     }
 }
