764490522d0f828555cf25fce6c79cfb48bc4f76
Trevor Pilley <trev@trevorpilley.co.uk>
2014-05-12 23:21:45 +0100

    Corrected DebuggerDisplay
    The committed and rolledback flags are now just private fields

--- MicroLite/Core/Transaction.cs
+++ MicroLite/Core/Transaction.cs
@@ -19,7 +19,7 @@ namespace MicroLite.Core
     /// <summary>
     /// The default implementation of <see cref="ITransaction"/>.
     /// </summary>
-    [System.Diagnostics.DebuggerDisplay("Active: {IsActive}, Committed: {WasCommitted}, RolledBack: {WasRolledBack}")]
+    [System.Diagnostics.DebuggerDisplay("Active: {IsActive}, Committed: {committed}, RolledBack: {rolledBack}")]
     internal sealed class Transaction : ITransaction
     {
         private static readonly ILog log = LogManager.GetCurrentClassLog();
