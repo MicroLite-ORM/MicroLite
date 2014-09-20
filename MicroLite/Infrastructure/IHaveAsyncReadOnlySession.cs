7cfc6f7a3330996c747825c2ce22e996aaf91379
Trevor Pilley <trev@trevorpilley.co.uk>
2014-09-20 12:17:02 +0100

    Closes #340
    Add IHaveAsyncSession and IHaveAsyncReadOnlySession infrastructure
    interfaces

---
+++ MicroLite/Infrastructure/IHaveAsyncReadOnlySession.cs
@@ -0,0 +1,33 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="IHaveAsyncReadOnlySession.cs" company="MicroLite">
+// Copyright 2012 - 2014 Project Contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// </copyright>
+// -----------------------------------------------------------------------
+namespace MicroLite.Infrastructure
+{
+#if NET_4_5
+
+    /// <summary>
+    /// An interface for classes which have an <see cref="IAsyncReadOnlySession"/> property.
+    /// </summary>
+    public interface IHaveAsyncReadOnlySession
+    {
+        /// <summary>
+        /// Gets or sets the asynchronous read-only MicroLite session.
+        /// </summary>
+        IAsyncReadOnlySession Session
+        {
+            get;
+            set;
+        }
+    }
+
+#endif
+}
