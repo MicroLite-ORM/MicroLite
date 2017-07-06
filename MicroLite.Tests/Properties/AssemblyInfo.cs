7d07f0943a9437ece42c7f469e99da4ffccb8039
Trevor Pilley <trev@trevorpilley.co.uk>
2017-07-06 21:45:04 +0100

    Built MicroLite 6.3.3

--- MicroLite.Tests/Properties/AssemblyInfo.cs
+++ MicroLite.Tests/Properties/AssemblyInfo.cs
@@ -30,6 +30,6 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 [assembly: AssemblyVersion("6.3.0.0")]
-[assembly: AssemblyFileVersion("6.3.2.0")]
+[assembly: AssemblyFileVersion("6.3.3.0")]
 [assembly: CLSCompliant(true)]
 [assembly: NeutralResourcesLanguageAttribute("en-GB")]
